44cbe92967e19b69a121da68150d8c9ab433b6d2
jin.tao <312090373@qq.com>
2020-01-14 17:22:42 +0800

    commit

--- src/view/home/modules/TopBanner.vue
+++ src/view/home/modules/TopBanner.vue
@@ -3,10 +3,10 @@
   <div class="top_box">
     <van-row type="flex" align="center">
       <van-col span="5" class="logo" v-show="businessInfo.hasBd">
-        <img src="@/assets/home/logo.png" alt />
+        <img :src="businessInfo.headerImg" alt />
       </van-col>
       <van-col span="8" class="title"  v-show="businessInfo.hasBd">
-        <div>大楚彩店</div>
+        <div>{{businessInfo.storeName}}</div>
         <img src="@/assets/home/wx.png" alt />
         <img src="@/assets/home/tel.png" alt />
         <img src="@/assets/home/share.png" alt />
@@ -32,51 +32,19 @@
         v-for="(item,index) in top_banner_list"
       >
         <div class="item">
-          <router-link :to="{path:'ranking',query:{}}">
+          <router-link :to="{path:'ranking',query:{'idx':index}}">
             <van-row type="flex" align="center">
               <van-col span="4">
-                <div class="t_txt tc">最高命中榜</div>
+                <div class="t_txt tc">{{index==0?'最高命中榜':index==1?'最高收益榜':'连红小生榜'}}</div>
               </van-col>
               <van-col span="20" class="re_box">
-                <van-row type="flex" class="re_item" align="center">
-                  <van-col span="2">1</van-col>
+                <van-row type="flex" class="re_item" align="center" v-for="(val,idx) in item">
+                  <van-col span="2">{{idx+1}}</van-col>
                   <van-col span="4">
-                    <img src="@/assets/home/game.png" alt />
+                    <img :src="val.header_img" alt />
                   </van-col>
-                  <van-col span="12">超级大神</van-col>
-                  <van-col span="5" class="tr">100%</van-col>
-                </van-row>
-                <van-row type="flex" class="re_item" align="center">
-                  <van-col span="2">2</van-col>
-                  <van-col span="4">
-                    <img src="@/assets/home/game.png" alt />
-                  </van-col>
-                  <van-col span="12">超级大神</van-col>
-                  <van-col span="5" class="tr">100%</van-col>
-                </van-row>
-                <van-row type="flex" class="re_item" align="center">
-                  <van-col span="2">3</van-col>
-                  <van-col span="4">
-                    <img src="@/assets/home/game.png" alt />
-                  </van-col>
-                  <van-col span="12">超级大神</van-col>
-                  <van-col span="5" class="tr">100%</van-col>
-                </van-row>
-                <van-row type="flex" class="re_item" align="center">
-                  <van-col span="2">4</van-col>
-                  <van-col span="4">
-                    <img src="@/assets/home/game.png" alt />
-                  </van-col>
-                  <van-col span="12">超级大神</van-col>
-                  <van-col span="5" class="tr">100%</van-col>
-                </van-row>
-                <van-row type="flex" align="center">
-                  <van-col span="2">5</van-col>
-                  <van-col span="4">
-                    <img src="@/assets/home/game.png" alt />
-                  </van-col>
-                  <van-col span="12">超级大神</van-col>
-                  <van-col span="5" class="tr">100%</van-col>
+                  <van-col span="12">{{val.nick_name}}</van-col>
+                  <van-col span="5" class="tr">{{index==0?val.seven_near_win_rate:index==1?val.seven_near_back_rate:val.this_red_count}}%</van-col>
                 </van-row>
               </van-col>
             </van-row>
@@ -91,25 +59,54 @@
   </div>
 </template>
 <script>
-import { rankList} from "@/api/api";
+import { rankList,getBusinessInfo} from "@/api/api";
 export default {
   name: "TopBanner",
   components: {},
   data() {
     return {
       initialSwipe: 0, //初始位置索引
-      top_banner_list: [1, 2, 3],
+	  top_banner_list: [[{"nick_name":"大神1","header_img":"/assets/home/game.png"},{"nick_name":"大神2"}],[{"nick_name":"大神1"},{"nick_name":"大神2"}],[{"nick_name":"大神1"},{"nick_name":"大神2"}]],
 	  businessInfo:{
 		  hasBd:false
 	  }
     };
   },
-
   methods: {
     onChange(index) {
       console.log(index);
     },
-    more() {}
+    more() {
+		 this.$router.push({path:'/ranking'});
+	},
+	loadRank(){
+		let map = {
+			"pageSize":5,
+			"day":7
+		}
+		rankList(map).then(res => {
+		  if(res.flag){//调用成功
+			  this.top_banner_list[0] = res.args.winList;
+			  this.top_banner_list[1] = res.args.backList;
+			  this.top_banner_list[2] = res.args.redList;
+		  }
+		}).catch(err=>{
+		  console.log(err);
+		});
+	},
+	loadBusinessInfo(){
+		rankList().then(res => {
+		  if(res.flag){//调用成功
+			  this.businessInfo.headerImg = res.args.headerImg;
+			  this.businessInfo.storeName = res.args.storeName;
+			  this.businessInfo.qrcode = res.args.qrcode;
+			  this.businessInfo.phone = res.args.phone;
+			  this.businessInfo.qmgdCode = res.args.qmgdCode;
+		  }
+		}).catch(err=>{
+		  console.log(err);
+		});
+	}
   }
 };
 </script>
